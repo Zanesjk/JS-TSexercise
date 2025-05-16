6731851192723a1b663383538bde8110d661d6be
Zanesjk <110065498+Zanesjk@users.noreply.github.com>
2025-05-16 17:16:29 +0800

    completed excercise 11

---
+++ index11/index.d.ts
@@ -0,0 +1,7 @@
+type StringDecorator = (input: string) => string;
+
+export const strReverse: StringDecorator;
+export const strToLower: StringDecorator;
+export const strToUpper: StringDecorator;
+export const strRandomize: StringDecorator;
+export const strInvertCase: StringDecorator;
